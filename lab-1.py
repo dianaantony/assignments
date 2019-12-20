257293244e4823c6eb30d71e1d8fcb7530ce0275
dianaantony <59092111+dianaantony@users.noreply.github.com>
2019-12-20 08:10:28 -0800

    Add files via upload

---
+++ lab-1.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+x="And let us consider how we may spur one another on toward love and good deeds, not giving up meeting together, as some are in the habit of doing, but encouraging one another—and all the more as you see the Day approaching."
+print(x)
+
+
+# In[10]:
+
+
+a='And let us consider how we may spur one another on toward love and good deeds, not giving up meeting together, as some are in the habit of doing, but encouraging one another—and all' the more as you see the Day approaching. Do not be afraid, little flock, for your Father has been pleased to give you the kingdom.'
+print(a)
+
+
+# In[12]:
+
+
+x="this is a test document and first lab excercise"
+print(x)
+
+
+# In[13]:
+
+
+x='this is a test document and first lab excercise'
+print(x)
+
+
+# In[18]:
+
+
+list=['Tuscany','Barcalona','Prague','Paris','Amsterdam']
+print(list)
+
+
+# In[19]:
+
+
+list.append('Venice')
+print(list)
+
+
+# In[20]:
+
+
+dict={
+    "Messi":'Football',
+    'Kohli':'Cricket',
+    'Marry kom':'Boxing',
+    'Sindhu':'Batminton',
+    'Fedrer':'Tennis'
+}
+print(dict)
+
+
+# In[22]:
+
+
+tup1 = ('A', 'B','C' ,'D');
+tup2 = (1, 2, 3, 4, 5, 6, 7 );
+print ("tup1[3]: ", tup1[3]);
+print ("tup2[1:5]: ", tup2[1:5]);
+
+
+# In[23]:
+
+
+del dict['Messi']
+print(dict)
+
+
+# In[24]:
+
+
+dict['Messi'] = 'football'
+print(dict)
+
+
+# In[25]:
+
+
+#Added forword slash before single quotes inside the string to avoid considering it while printing 
+x='And let us consider how we may spur one another on toward love and good deeds, not giving up meeting together, as some are in the habit of doing, \'but encouraging one another—and all the more as you see the Day approaching'
+print(x)
+
+
+# In[ ]:
+
+
+
+
